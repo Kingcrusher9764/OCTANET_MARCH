e0073bdab733d4e192cc0a5923817307fa5fd902
Kingcrusher <tiwarivikrant9764@gmail.com>
2024-03-07 16:12:41 +0530

    first commit

---
+++ Landing page/js/index.js
@@ -0,0 +1,60 @@
+// Show menu
+const navMenu = document.getElementById("nav-menu")
+const navToggle = document.getElementById("nav-toggle")
+const navClose = document.getElementById("nav-close")
+
+if(navToggle){
+    navToggle.addEventListener("click", ()=>{
+        navMenu.classList.add("show-menu")
+    })
+}
+if(navClose){
+    navClose.addEventListener("click", ()=>{
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+// remove menu
+const navLink = document.querySelectorAll(".nav__link")
+
+const linkAction = ()=>{
+    navMenu.classList.remove("show-menu")
+}
+navLink.forEach(n=>n.addEventListener("click", linkAction))
+
+// change background header
+const blurHeader = ()=>{
+    const header = document.getElementById("header")
+    this.scrollY >= 50 ? header.classList.add("blur-header") : header.classList.remove("blur-header")
+
+}
+window.addEventListener("scroll", blurHeader)
+
+// show scrollup
+const scrollUp = ()=>{
+    const scrollup = document.getElementById("scroll-up")
+    this.scrollY >= 350 ? scrollup.classList.add("show-scroll"): scrollup.classList.remove("show-scroll")
+
+}
+window.addEventListener("scroll", scrollUp)
+
+// scroll section active link
+const sections = document.querySelectorAll("section[id]")
+
+const scrollActive = ()=>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 58
+        const sectionId = current.getAttribute("id")
+        const sectionsClass = document.querySelector(".nav__menu a[href*="+sectionId+"]")
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add("active-link")
+        }else{
+            sectionsClass.classList.remove("active-link")
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive)
